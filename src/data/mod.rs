f3ff5f63010c9e56b5d1bf1fe71d9221b9bcc359
Ryan <ryan@alien.gov>
2025-07-08 23:56:55 +0800

    feat: (WIP) Add support for Organized data structure

--- src/data/mod.rs
+++ src/data/mod.rs
@@ -1,14 +1,78 @@
+use color_eyre::eyre::Report;
+use std::fmt::Display;
+
 use color_eyre::Result;
+use reqwest::Url;
 pub mod raw_data;
+pub mod schedules;
 pub mod translation_data;
 
-pub fn fetch_data() -> Result<raw_data::Data> {
-    todo!();
+#[derive(Debug, Clone)]
+enum DataError {
+    TranslationError(String),
 }
 
-pub fn translate_data(
-    orig_data: raw_data::Data,
-    translation: translation_data::TranslationData,
-) -> Result<raw_data::Data> {
-    todo!();
+impl Display for crate::data::DataError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DataError::TranslationError(id) => {
+                write!(f, "Failed to find translation to the word with id {}", id)
+            }
+        }
+    }
+}
+
+impl std::error::Error for DataError {}
+
+async fn fetch_data() -> Result<raw_data::RawData> {
+    let res: String = reqwest::get("https://splatoon3.ink/data/schedules.json")
+        .await?
+        .text()
+        .await?;
+    let res: raw_data::RawData = serde_json::from_str(&res)?;
+
+    Ok(res)
+}
+
+async fn fetch_translation(
+    lang: String,
+) -> Result<translation_data::FlattenedTranslationDictionary> {
+    let base_url: Url = Url::parse("https://splatoon3.ink/data/locale/")?;
+    let joined_url: Url = base_url.join(&lang)?;
+
+    let res: String = reqwest::get(joined_url).await?.text().await?;
+    let res: translation_data::TranslationData = serde_json::from_str(&res)?;
+    let res: translation_data::FlattenedTranslationDictionary = res.into();
+
+    Ok(res)
+}
+
+fn lookup_translation(
+    id: String,
+    dictionary: &translation_data::FlattenedTranslationDictionary,
+) -> Result<String> {
+    dictionary
+        .get(&id)
+        .ok_or_else(|| Report::new(DataError::TranslationError(id)))
+        .cloned()
+}
+
+#[cfg(test)]
+mod test {
+    use super::{lookup_translation, translation_data::FlattenedTranslationDictionary};
+
+    #[test]
+    fn test_translation_lookup_with_dictionary() {
+        let dict = FlattenedTranslationDictionary::from([
+            ("VnNTdGFnZS0x".to_string(), "温泉花大峡谷".to_string()),
+            ("VnNTdGFnZS0y".to_string(), "鳗鲶区".to_string()),
+        ]);
+
+        assert_eq!(
+            lookup_translation("VnNTdGFnZS0x".to_string(), &dict).unwrap(),
+            "温泉花大峡谷".to_string()
+        );
+
+        assert!(lookup_translation("nonexistent".to_string(), &dict).is_err());
+    }
 }
---
+++ src/data/schedules.rs
@@ -0,0 +1,117 @@
+use chrono::{self, DateTime, Utc};
+
+use super::raw_data;
+pub struct Schedules {
+    pub regular: Vec<Schedule>,
+    pub anarchy_open: Vec<Schedule>,
+    pub anarchy_series: Vec<Schedule>,
+    pub x_battle: Vec<Schedule>,
+}
+
+pub struct Schedule {
+    pub start_time: DateTime<Utc>,
+    pub end_time: DateTime<Utc>,
+    pub stages: Vec<Stage>,
+    pub rule: Rule,
+}
+
+pub struct Stage {
+    pub name: String,
+    pub id: String,
+}
+
+pub struct Rule {
+    pub name: String,
+    pub id: String,
+}
+
+impl From<&raw_data::MatchVsStage> for Stage {
+    fn from(value: &raw_data::MatchVsStage) -> Self {
+        Stage {
+            name: value.name.clone(),
+            id: value.id.clone(),
+        }
+    }
+}
+
+impl From<&raw_data::MatchVsRule> for Rule {
+    fn from(value: &raw_data::MatchVsRule) -> Self {
+        Rule {
+            name: value.name.clone(),
+            id: value.id.clone(),
+        }
+    }
+}
+
+impl From<&raw_data::MatchNode> for Schedule {
+    fn from(value: &raw_data::MatchNode) -> Self {
+        Schedule {
+            start_time: value.start_time,
+            end_time: value.end_time,
+            stages: value
+                .match_setting
+                .vs_stages
+                .iter()
+                .map(|stage| stage.into())
+                .collect(),
+            rule: (&value.match_setting.vs_rule).into(),
+        }
+    }
+}
+
+impl From<raw_data::RawData> for Schedules {
+    fn from(value: raw_data::RawData) -> Self {
+        let mut res = Schedules {
+            regular: vec![],
+            anarchy_open: vec![],
+            anarchy_series: vec![],
+            x_battle: vec![],
+        };
+
+        value
+            .data
+            .regular_schedules
+            .nodes
+            .iter()
+            .for_each(|schedule| {
+                res.regular.push(schedule.into());
+            });
+
+        value.data.x_schedules.nodes.iter().for_each(|schedule| {
+            res.x_battle.push(schedule.into());
+        });
+
+        value
+            .data
+            .bankara_schedules
+            .nodes
+            .iter()
+            .for_each(|schedule| {
+                for setting in &schedule.match_settings {
+                    let schedule_res = Schedule {
+                        start_time: schedule.start_time,
+                        end_time: schedule.end_time,
+                        stages: (setting
+                            .match_setting
+                            .vs_stages
+                            .iter()
+                            .map(|stage| stage.into())
+                            .collect()),
+                        rule: (&setting.match_setting.vs_rule).into(),
+                    };
+                    match setting.bankara_mode {
+                        raw_data::BankaraMode::Open => {
+                            res.anarchy_open.push(schedule_res);
+                        }
+                        raw_data::BankaraMode::Challenge => {
+                            res.anarchy_series.push(schedule_res);
+                        }
+                    }
+                }
+            });
+
+        res
+    }
+}
+
+//TODO: Add tests
